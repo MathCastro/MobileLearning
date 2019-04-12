89b50b102f3441dbfdf98f9052451c907ba9b0c5
MathCastro <matheusfk74@gmail.com>
2019-04-12 11:43:27 -0300

    listScreen and background

--- android/app/src/main/java/com/example/exercicio/view/ListScreen.kt
+++ android/app/src/main/java/com/example/exercicio/view/ListScreen.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import com.example.exercicio.R
 import com.example.exercicio.model.UserBO
 
@@ -13,29 +16,51 @@ class ListScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_screen)
+
+        var options: Array<String> = resources.getStringArray(R.array.options_array)
+
+        val prodAdapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, options)
+
+        val list: ListView = findViewById(R.id.initial_list)
+
+        list.adapter = prodAdapter
+
+        list.setOnItemClickListener { parent, view, position, id ->
+            if(position == 0)
+                calendar()
+            else if(position == 1)
+                calculator()
+            else if(position == 2)
+                idiom()
+            else if(position == 3)
+                tabBar()
+            else
+                text()
+        }
     }
 
-    fun calendar(view: View) {
+    fun calendar(view: View? = null) {
         val intent = Intent(this, CalendarActivity::class.java)
         startActivity(intent)
     }
 
-    fun calculator(view: View) {
+    fun calculator(view: View? = null) {
         val intent = Intent(this, CalculatorActivity::class.java)
         startActivity(intent)
     }
 
-    fun idiom(view: View) {
+    fun idiom(view: View? = null) {
         val intent = Intent(this, IdiomActivity::class.java)
         startActivity(intent)
     }
 
-    fun tabBar(view: View) {
+    fun tabBar(view: View? = null) {
         val intent = Intent(this, TabBarActivity::class.java)
         startActivity(intent)
     }
 
-    fun text(view: View) {
+    fun text(view: View? = null) {
         val intent = Intent(this, TextActivity::class.java)
         startActivity(intent)
     }
